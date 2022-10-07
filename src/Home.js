500b5f93971f2dd426d8fc2e582ee8fa1abd67e0
Ik3nna <iamiykendukwu@gmail.com>
2022-10-07 19:54:13 +0100

    responsive for pc and tabs

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import data from "./data";
 
 const Home = ()=> {
-
     return(
         <main>
             {data.map((item)=>{
@@ -12,6 +11,47 @@ const Home = ()=> {
                     <section key={id}>
                         <article className="container">
                             <img src={logo} alt={company} className="img" />
+
+                            <div className="sub-section">
+                                <div>
+                                    <span>{company}</span>
+                                    <span className={`${newItem === true && "bg"}`}>{newItem ? "new!" : null}</span>
+                                    <span className={`${featured === true && "ft"}`}>{featured ? "featured": null}</span>
+                                </div>
+                                
+                                <div className="position">
+                                    {position}
+                                </div>
+
+                                <div>
+                                    <span>{postedAt}</span><span>.</span>
+                                    <span>{contract}</span><span>.</span>
+                                    <span>{location}</span>
+                                </div>
+                            </div>
+
+                            <div className="tags">
+                                <span className="role">{role}</span>
+                                <span className="level">{level}</span>
+                                <span className="languages">
+                                    {languages.map((lg,index)=>{
+                                        return(
+                                            <span key={index} className="sub">
+                                                {lg}
+                                            </span>
+                                        );
+                                    })}
+                                </span>
+                                <span>
+                                    {tools.map((tool,index)=>{
+                                        return(
+                                            <span key={index} className="tls">
+                                                {tool}
+                                            </span>
+                                        );
+                                    })}
+                                </span>
+                            </div>
                             
                         </article>
                     </section>
